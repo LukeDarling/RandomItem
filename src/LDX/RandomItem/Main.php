512b8fa83d810e501de3d0d4c59766264340a9b1
Luke Darling <luke@lukedarling.net>
2016-03-09 15:59:38 -0500

    Fixed stuff

--- src/LDX/RandomItem/Main.php
+++ src/LDX/RandomItem/Main.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace LDX\RandomItem;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
@@ -7,19 +9,22 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\item\Item;
+
 class Main extends PluginBase {
+
   public function onEnable() {
     $this->saveDefaultConfig();
     $c = $this->getConfig()->getAll();
-    $t = $c["interval"] * 1200;
+    $t = $c["Interval"] * 1200;
     $num = 0;
-    foreach ($c["items"] as $i) {
+    foreach ($c["Items"] as $i) {
       $r = explode(":",$i);
       $this->itemdata[$num] = array("id" => $r[0],"meta" => $r[1],"amount" => $r[2]);
       $num++;
     }
     $this->getServer()->getScheduler()->scheduleRepeatingTask(new Gift($this),$t);
   }
+
   public function onCommand(CommandSender $issuer,Command $cmd,$label,array $args) {
     if((strtolower($cmd->getName()) == "gift") && isset($args[0])) {
       if($this->getServer()->getPlayer($args[0]) instanceof Player) {
@@ -38,12 +43,14 @@ class Main extends PluginBase {
       return false;
     }
   }
+
   public function give($p,$data) {
     if($p instanceof Player && ($p->hasPermission("randomitem") || $p->hasPermission("randomitem.receive"))) {
       $item = new Item($data["id"],$data["meta"],$data["amount"]);
       $p->getInventory()->addItem($item);
     }
   }
+
   public function giveAll() {
     $data = $this->generateData();
     $this->broadcast("Random item given! (" . $data["id"] . ":" . $data["meta"] . ")");
@@ -51,14 +58,16 @@ class Main extends PluginBase {
       $this->give($p,$data);
     }
   }
+
   public function broadcast($m) {
     foreach($this->getServer()->getOnlinePlayers() as $p) {
       $p->sendMessage($m);
     }
     $this->getLogger()->info(TextFormat::YELLOW . $m);
   }
+
   public function generateData() {
-    return $this->itemdata[rand(0,(count($this->itemdata) - 1))];
+    return $this->itemdata[mt_rand(0,(count($this->itemdata) - 1))];
   }
+
 }
-?>
